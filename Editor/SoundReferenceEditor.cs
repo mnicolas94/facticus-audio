b4591431915f101cb2a697f3c84d5bbcc7f53c59
mnicolas94 <miguelcok27@gmail.com>
2024-06-04 16:40:57 -0400

    feat: add button to SoundReference inspector

---
+++ Editor/SoundReferenceEditor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Utils.Editor;
+
+namespace Audio.Editor
+{
+    [UnityEditor.CustomEditor(typeof(SoundReference))]
+    public class SoundReferenceEditor : UnityEditor.Editor
+    {
+        [SerializeField] private SoundReference _target;
+        
+        public override VisualElement CreateInspectorGUI()
+        {
+            _target = (SoundReference)target;
+            var root = new VisualElement();
+            
+            // draw default inspector
+            var serializedProperties = PropertiesUtils.GetSerializedProperties(serializedObject);
+            foreach (var serializedProperty in serializedProperties)
+            {
+                var propertyField = new PropertyField(serializedProperty);
+                root.Add(propertyField);
+            }
+
+            // add button
+            var playButton = new Button(PlayAudio);
+            playButton.text = "Play";
+            root.Add(playButton);
+            
+            return root;
+        }
+
+        private void PlayAudio()
+        {
+            _target.PlayAudio();
+        }
+    }
+}
